a595868de3adc0917e952573bfe31bd8f92f8167
Paul <workmepaul@gmail.com>
2024-11-01 02:26:29 +0300

    api

--- client/src/api/ContactService.ts
+++ client/src/api/ContactService.ts
@@ -8,7 +8,7 @@ export interface IContact {
 
 export default class ContactService {
 	static async postContact(name: string, email: string, message: string) {
-		const response = await axios.post('http://localhost:3000/api/contact', {name, email, message})
+		const response = await axios.post('http://84.252.135.177:3000/api/contact', {name, email, message})
 		return response
 	}
 }
